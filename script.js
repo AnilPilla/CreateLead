74fc27c90c35d5880e2f651f170c137659e6a0cf
Anil <anilpilla.ds@gmail.com>
2024-11-07 21:01:17 -0800

    Added re-captcha

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function beforeSubmit() {
-    debugger
-    let outputDate  = document.querySelector(".outputDate")
-    let inputDate  = document.querySelector(".inputDate")
-    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US")
-    outputDate.value = formattedDate
-    console.log(formattedDate)
+  debugger;
+  let outputDate = document.querySelector(".outputDate");
+  let inputDate = document.querySelector(".inputDate");
+  let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
+  outputDate.value = formattedDate;
+  console.log(formattedDate);
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+} setInterval(timestamp, 500);
